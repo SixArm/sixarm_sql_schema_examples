6c36951bb5ec56c1b4f3a332ffca75d572eb10b9
Joel Parker Henderson <joel@joelparkerhenderson.com>
2017-01-06 02:32:12 -0800

    Add altitude

--- geolocation.sql
+++ geolocation.sql
@@ -5,8 +5,9 @@
 --changeset sixarm@sixarm.com:1
 create table geolocation (
   id int not null primary key,
-  latitude float,                -- example: 37.8199° N 
-  longitude float,               -- example: 122.4783° W
-  elevation float,               -- example: 67.056 meters
+  latitude float,                -- example: 37.8199° N (Golden Gate Bridge)
+  longitude float,               -- example: 122.4783° W (Golden Gate Bridge)
+  altitude float,                -- example: 67.056 meters (to local surface of the earth)
+  elevation float,               -- example: 67.056 meters (to global sea level)
 );
 --rollback drop table geolocation;
