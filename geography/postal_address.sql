7dd11b9f403e45072861b02889f840aabfb77c77
Joel Parker Henderson <joel@joelparkerhenderson.com>
2018-12-27 14:19:07 -0800

    Add street to postal address

--- geography/postal_address.sql
+++ geography/postal_address.sql
@@ -11,6 +11,7 @@ create table postal_address (
   postal_region_id uuid references region, -- example: California
   locality_id uuid references locality, -- example: San Francisco
   postcode_id uuid references postcode, -- example: 94101
+  street_id uuid references street, -- example: Main Street
   name_as_en varchar, -- example: Acme Company
   name_as_es varchar, -- example: Acme Empressa
   name_as_fr varchar, -- example: Acme Entreprise
