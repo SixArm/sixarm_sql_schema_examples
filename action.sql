1793d5f07f37df5f7c087fd2259c5a48e3bb038c
Joel Parker Henderson <joel@joelparkerhenderson.com>
2018-12-27 14:38:35 -0800

    Add more timing options for actions and events

--- action.sql
+++ action.sql
@@ -10,9 +10,16 @@ create table action ( --see https://schema.org/Action
   name_as_en varchar, -- example: sing at a music concert
   name_as_es varchar, -- example cantar en un concierto de musica
   name_as_fr varchar, -- example: chanter à concert de musique
-  start_when timestamp, -- example: 2000-01-01T12:00:00Z
+  start_when timestamp, -- example: 2000-01-01TZ
+  start_date_local date, -- example: : 2000-01-01
+  start_time_local time, -- example: 12:00:00
   stop_when timestamp, -- example: 2000-01-02T12:00:00Z
+  stop_date_local date, -- example: : 2000-01-01
+  stop_time_local time, -- example: 12:00:00
+  duration integer, -- example: 86400 seconds a.k.a. 1 day
   agent_person_id uuid references person, -- example: Alice
+  agent_place_id uuid references place, -- example: Golden Gate Park
+  agent_thing_id uuid references thing, -- example: hammer
   agent_org_id uuid references org, --  example: Acme
 );
 
--- event.sql
+++ event.sql
@@ -10,8 +10,16 @@ create table event ( --see https://schema.org/Event
   name_as_en varchar, -- example: music concert
   name_as_es varchar, -- example: concierto de musica
   name_as_fr varchar, -- example: concert de musique
-  start_when timestamp, -- example: 2000-01-01T12:00:00Z
+  description_as_en varchar, -- example: music concert
+  description_as_es varchar, -- example: concierto de musica
+  description_as_fr varchar, -- example: concert de musique
+  start_when timestamp, -- example: 2000-01-01TZ
+  start_date_local date, -- example: : 2000-01-01
+  start_time_local time, -- example: 12:00:00
   stop_when timestamp, -- example: 2000-01-02T12:00:00Z
+  stop_date_local date, -- example: : 2000-01-01
+  stop_time_local time, -- example: 12:00:00
+  duration integer -- example: 86400 seconds a.k.a. 1 day
 );
 
 --rollback drop table event;
