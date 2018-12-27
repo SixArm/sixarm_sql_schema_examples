b906f77d4b97097743206d528238803f0e21179a
Joel Parker Henderson <joel@joelparkerhenderson.com>
2018-12-27 14:32:59 -0800

    Fix language in event

--- event.sql
+++ event.sql
@@ -8,6 +8,7 @@
 create table event ( --see https://schema.org/Event
   id uuid not null primary key,
   name_as_en varchar, -- example: music concert
+  name_as_es varchar, -- example: concierto de musica
   name_as_fr varchar, -- example: concert de musique
   start_when timestamp, -- example: 2000-01-01T12:00:00Z
   stop_when timestamp, -- example: 2000-01-02T12:00:00Z
