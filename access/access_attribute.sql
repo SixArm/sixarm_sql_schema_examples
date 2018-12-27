b63e32a8cd20984d5c5b196b9c5507581c856f2c
Joel Parker Henderson <joel@joelparkerhenderson.com>
2018-12-27 13:18:35 -0800

    Fix text type to varchar

--- access/access_attribute.sql
+++ access/access_attribute.sql
@@ -7,9 +7,9 @@
 
 create table access_attribute (
   id uuid not null primary key,
-  name_as_en text unique, -- example: "administrator"
-  name_as_es text unique, -- example: "administrador"
-  name_as_fr text unique -- example: "administrateur"
+  name_as_en varchar unique, -- example: "administrator"
+  name_as_es varchar unique, -- example: "administrador"
+  name_as_fr varchar unique -- example: "administrateur"
 );
 
 --rollback drop table access_attribute;
