474c27e1fedd6df1afcd8d9d1e03b13c65015054
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-05-16 23:29:03 -0700

    Drop LEI from person

--- examples/person.sql
+++ examples/person.sql
@@ -28,7 +28,6 @@ create table person (
   vcard_vcf text, -- https://en.wikipedia.org/wiki/VCard
   hcard_html text, -- https://en.wikipedia.org/wiki/HCard
   value_added_tax_identification_number varchar, -- https://schema.org/vatID https://en.wikipedia.org/wiki/VAT_identification_number
-  legal_entity_identifier char(20), -- https://schema.org/leiCode https://en.wikipedia.org/wiki/Legal_Entity_Identifier
   international_standard_of_industrial_classification_rev_4 char(5), -- https://schema.org/isicV4 https://en.wikipedia.org/wiki/International_Standard_Industrial_Classification
   lifespan_id uuid references lifespan -- example: birth date is 2000-01-01
 );
